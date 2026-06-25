b5509df53378cf13680a8de29f7c516618ce075f
Krish Kumar <krishkumar.work3@gmail.com>
2026-06-25 07:38:37 +0530

    add todo request and response DTOs

--- src/models.rs
+++ src/models.rs
@@ -13,3 +13,21 @@ pub struct Todo {
     pub created_at: DateTime<Utc>,
     pub updated_at: DateTime<Utc>,
 }
+
+/// Request DTO for creating a new todo
+#[derive(Debug, Deserialize)]
+pub struct CreateTodo {
+    pub title: String,
+    pub description: Option<String>,
+}
+
+/// Request DTO for updating an existing todo
+#[derive(Debug, Deserialize)]
+pub struct UpdateTodo {
+    pub title: Option<String>,
+    pub description: Option<String>,
+    pub completed: Option<bool>,
+}
+
+/// Response DTO for todo operations
+pub type TodoResponse = Todo;
