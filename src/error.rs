1465392cd9e813679cbc4186b9f59e9514a44a98
Krish Kumar <krishkumar.work3@gmail.com>
2026-06-26 07:38:04 +0530

    Add error response model

--- src/error.rs
+++ src/error.rs
@@ -6,3 +6,9 @@ use axum::{
 use serde::{Deserialize, Serialize};
 use sqlx::Error as SqlxError;
 use std::fmt;
+
+#[derive(Debug, Serialize, Deserialize)]
+pub struct ErrorResponse {
+    pub status: String,
+    pub message: String,
+}
