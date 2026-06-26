051ccd8f295138dfc912df3104dd7874c2853333
Krish Kumar <krishkumar.work3@gmail.com>
2026-06-26 07:59:02 +0530

    Add authentication error messages

--- src/error.rs
+++ src/error.rs
@@ -63,3 +63,24 @@ impl ErrorMessage {
             ErrorMessage::TodoNotFound => "Todo not found".to_string(),
             ErrorMessage::TodoValidationError => "Validation error".to_string(),
             ErrorMessage::TodoAlreadyCompleted => "Todo is already completed".to_string(),
+            ErrorMessage::WrongCredentials => "Email or password is wrong".to_string(),
+            ErrorMessage::EmailExist => "A user with this email already exists".to_string(),
+            ErrorMessage::UserNoLongerExist => {
+                "User belonging to this token no longer exists".to_string()
+            }
+            ErrorMessage::EmptyPassword => "Password cannot be empty".to_string(),
+            ErrorMessage::HashingError => "Error while hashing password".to_string(),
+            ErrorMessage::InvalidHashFormat => "Invalid password hash format".to_string(),
+            ErrorMessage::ExceededMaxPasswordLength(max_length) => {
+                format!("Password must not be more than {} characters", max_length)
+            }
+            ErrorMessage::InvalidToken => "Authentication token is invalid or expired".to_string(),
+            ErrorMessage::TokenNotProvided => {
+                "You are not logged in, please provide a token".to_string()
+            }
+            ErrorMessage::UserNotAuthenticated => {
+                "Authentication required. Please log in.".to_string()
+            }
+        }
+    }
+}
